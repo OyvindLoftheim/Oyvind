cb4bbd68505b6dcbd57f7c8292d04de586a49678
OyvindLoftheim <147072984+OyvindLoftheim@users.noreply.github.com>
2023-10-05 19:12:05 +0200

    Lagt til Floating
    Lagt til Floating-knapper

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Stack } from "@mui/material";
 import Profile from "./components/Profile";
 import Description from "./components/Description";
 import ContactForm from "./components/ContactForm";
+import SomeComponent from "./components/SomeComponent";
+import FloatingActionButtonExtendedSize from "./components/Floating";
 
 export default function App() {
   return (
     <Stack maxWidth="sm" direction="column" spacing={2} margin="auto" p={2}>
       <Profile />
       <Description />
+      <SomeComponent />
+      <FloatingActionButtonExtendedSize />
       <ContactForm />
-      <footer />
     </Stack>
   );
+}
---
+++ src/components/SomeComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+function SomeComponent() {
+  return (
+    <Box py={5} bgcolor="#a85932" borderRadius={3} boxShadow={3} mt={4} px={3}>
+      <Typography variant="h6" gutterBottom color="#0c0c0d">
+        Om Meg
+      </Typography>
+      <Typography>
+        Dette er min første nettside laget fra bunnen.
+      </Typography>
+    </Box>
+  );
+}
+
+export default SomeComponent;
